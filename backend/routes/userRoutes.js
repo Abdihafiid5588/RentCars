2d40bb84e7ba6b61e41bc7a1ec6a529bf278b42a
yasmina <jasminsidii@gmail.com>
2025-05-08 03:15:04 -0700

    error xaliyey

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,10 +7,4 @@ router.post("/login", loginUser);
 const { getProfile } = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 
-router.get("/profile", protect, getProfile);
-
-
-
-
-
 module.exports = router;
